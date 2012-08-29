77464e3b0ff91849415d97ec9327675c6dd25b2e
Harry Cheng <chengyuhui1@gmail.com>
2012-08-29 18:32:51 +0800

    Fix some errors,updated some javascript and css files.

--- static/js/core.js
+++ static/js/core.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * The entry of scripting contain menu building.
  * Licensed under Free-BSD
@@ -184,243 +183,31 @@ function dropDownTagsSelectorForPop() {
     });
 }
 
-function convertTags() {
-    $(".work_tags, #tags_selector_for_pop, #tags_selector_for_new").convertToTags({
-        "click":function(tag_frame, event){
-            var data = tag_frame.attr("data");
-            console.log(data);
+function page_handle (page_num, paginator) {
+    // console.log(paginator);
+    paginator.goToPage(page_num);
+    $(".page_button_wrapper").convertToButton();
+    var paginator_width = 0;
+    paginator.children().each(function() {
+        var child = $(this);
+        
+        if(child.hasClass("page_ellipsis_wrapper")) {
+            paginator_width+= child.width() + 21;
+        } else {
+            paginator_width+= child.width()+8;
         }
+        // console.log(paginator_width);
     });
+    paginator_width += 20;
+    // console.log(paginator_width);
+    paginator.css({"width":  paginator_width+"px", "margin-left": "auto", "margin-right": "auto"});
 }
 
 function layoutWorkPanel() {
-    //Add margins to work panel, but not the last one of each row.
-    $(".works_panel_wrapper").children(".works_panel").each(function(index, item) {
-        if( (index+1) % 4 != 0 ) {
-            $(item).addClass("works_panel_margin");
-        }
-    });
-    //Add slide animation to preview image light box controls.
-    $(".work_content_wrapper div.preview_img_wrapper").hover(function(event) {
-        $(this).find("div.img_lightbox_controls").slideDown("fast");
-    }, function(event) {
-        $(this).find("div.img_lightbox_controls").slideUp("fast");
-    });
-    //convert the more content link to a relief button.
-    $(".more_content_button_wrapper").convertToButton();
-    //Apply FancyBox to light box controls
-    $(".zoom_in_tool").fancybox();
-
-}
-
-$(document).ready(function(){
-    layoutWorkPanel();
-    navMenuBuilding();
-    dropDownTimeSelectorForPop();
-    dropDownPlatformSelectorForPop();
-    dropDownTagsSelectorForPop();
-    convertTags();
-=======
-<<<<<<< HEAD
-/**
- * The entry of scripting contain menu building.
- * Licensed under Free-BSD
- * Author: Delacstrasz
- * Date: July 15th, 2012
- * Version: 1.0
- */
-
-//lock the menu hover effect when this menu is the  current page
-(function($, undefined){
-    // prevent duplicate loading
-    // this is only a problem because we proxy existing functions
-    // and we don't want to double proxy them
-    $.simpleMenu = $.simpleMenu || {};
-
-    if($.simpleMenu.version){
-        return;
-    }
-
-    $.extend($.simpleMenu, {
-        version: "1.00",
-        hrefList: {},
-        returnDelay: 100
-    });
-
-    $.fn.extend({
-        lockedMenu:function (returnDelay) {
-            $.simpleMenu.returnDelay = returnDelay || $.simpleMenu.returnDelay;
-            var currentPath = window.location.pathname;
-            var currentURL = currentPath.substring(currentPath.lastIndexOf("/"), currentPath.length);
-            currentURL = currentURL || "/index.html";
-            //consider our default index.html is root /,so if the path is /index.html. convert to root
-            currentURL = currentURL=="/index.html" ? "/" : currentURL;
-            $.extend($.simpleMenu, {
-                defaultURL: currentURL
-            });
-            var menuElement = this;
-            this.find("a").each(function(index,domElement){
-                var $anchor = $(domElement);
-                var href = $anchor.attr("href");
-                if(href==currentURL) {
-                    $.simpleMenu.hrefList[ href ] = true;
-                    $anchor.addClass("hot");
-                } else {
-                    $.simpleMenu.hrefList[ href ] = false;
-                    $anchor.hover(function(event){
-                        activeMenuItem(event, menuElement);
-                    },function(event){
-                        deactiveMenuItem(event, menuElement);
-                    });
-                }
-            });
-        }
-    });
-
-    function activeMenuItem (event, menuElement) {
-        var target = event.target;
-        var $anchor = $(target);
-        $.simpleMenu.hrefList[ $anchor.attr("href") ] = true;
-        $anchor.addClass("hot");
-        // $.simpleMenu.hrefList[$.simpleMenu.defaultURL] = false;
-        // menuElement.find("a[href='"+$.simpleMenu.defaultURL+"']").removeClass("hot");
-        menuElement.find("a[href!='"+$anchor.attr("href")+"']").each(function(i, item){
-            $(item).removeClass("hot");
-            $.simpleMenu.hrefList[$(item).attr("href")] = false;
-        });
-        // console.log(target);
-    }
-
-    function deactiveMenuItem (event, menuElement) {
-        var target = event.target;
-        var $anchor = $(target);
-        $.simpleMenu.hrefList[ $anchor.attr("href")] = false;
-        $anchor.removeClass("hot");
-        setTimeout(function() {
-            var isMouseLeft = true;
-            $.each($.simpleMenu.hrefList, function(index, item){
-                if(item){
-                    isMouseLeft = false;
-                }
-            });
-            if(isMouseLeft) {
-                $.simpleMenu.hrefList[$.simpleMenu.defaultURL] = true;
-                menuElement.find("a[href='"+$.simpleMenu.defaultURL+"']").addClass("hot");
-            }
-        }, $.simpleMenu.returnDelay);
-    }
-
-})(jQuery);
-
-function navMenuBuilding() {
-    $("#nav_menu").lockedMenu();
-    $("#nav_menu").lavaLamp({
-        speed: 300,
-        click: function(){
-
-        },
-        starItem : 1
-    });
-}
-
-function dropDownTimeSelectorForPop() {
-    var api = "/api/time";
-    $("#time_selector_for_pop").jDropDownControl({
-        "click":function(index, event) {
-            var target = event.target;
-            var data = $(target).attr("data");
-            console.log(data);
-            // var time = $.parseJSON(data);
-            $("#time_selector_for_pop div.drop_down_wrapper").children("span").text($(target).text());
-            if(data){
-                // window.location = api+"?time="+data;
-                
-            }
-        }
-    });
-    $("#time_selector_for_new").jDropDownControl({
-        "click":function(index, event) {
-            var target = event.target;
-            var data = $(target).attr("data");
-            console.log(data);
-            // var time = $.parseJSON(data);
-            $("#time_selector_for_new div.drop_down_wrapper").children("span").text($(target).text());
-            if(data){
-                // window.location = api+"?time="+data;
-                
-            }
-        }
-    });
-}
-
-function dropDownPlatformSelectorForPop() {
-    var api="/api/platform";
-    $("#platform_selector_for_pop").jDropDownControl({
-        "click":function(index, event) {
-            var target = event.target;
-            var data = $(target).attr("data");
-            console.log(data);
-            $("#platform_selector_for_pop div.drop_down_wrapper").children("span").text($(target).text());
-            if(data) {
-                // window.location = api+"?platform="+data;
-            }
-        }
-    });
-    $("#platform_selector_for_new").jDropDownControl({
-        "click":function(index, event) {
-            var target = event.target;
-            var data = $(target).attr("data");
-            console.log(data);
-            $("#platform_selector_for_new div.drop_down_wrapper").children("span").text($(target).text());
-            if(data) {
-                // window.location = api+"?platform="+data;
-            }
-        }
-    });
-}
-
-function dropDownTagsSelectorForPop() {
-    var api="/api/tags";
-    $("#tags_selector_for_pop").jDropDownControl({
-        "click":function(index, event) {
-            var target = event.target;
-            var data = $(target).attr("data");
-            console.log(data);
-            if(data) {
-                // window.location = api+"?platform="+data;
-            }
-        },
-        "width": "200px",
-        "defaultLabel": "TAGs"
-    });
-    $("#tags_selector_for_new").jDropDownControl({
-        "click":function(index, event) {
-            var target = event.target;
-            var data = $(target).attr("data");
-            console.log(data);
-            if(data) {
-                // window.location = api+"?platform="+data;
-            }
-        },
-        "width": "200px",
-        "defaultLabel": "TAGs"
-    });
-}
-
-function convertTags() {
-    $(".work_tags, #tags_selector_for_pop, #tags_selector_for_new").convertToTags({
-        "click":function(tag_frame, event){
-            var data = tag_frame.attr("data");
-            console.log(data);
-        }
-    });
-}
-
-function layoutWorkPanel() {
-    // var getInt = function (pxValue) {
-    //     var pattern = /\d+/g;
-    //     return parseInt(pxValue.match(pattern)[0]);
-    // };
+    var getInt = function (pxValue) {
+        var pattern = /\d+/g;
+        return parseInt(pxValue.match(pattern)[0]);
+    };
     //Add margins to work panel, but not the last one of each row.
     $(".works_panel_wrapper").children(".works_panel").each(function(index, item) {
         if( (index+1) % 4 != 0 ) {
@@ -439,209 +226,44 @@ function layoutWorkPanel() {
     $(".zoom_in_tool").fancybox();
 
     var maxWords = 16;
-    // aa="Memories Off 7 秋之回忆7 打勾勾的记忆";
-    // console.log(aa.length);
-    // console.log(aa.substr(0, 16));
     //trim the long title to fit the size of work panel
     var workTitleWidth = $(".work_title").width();
     
     $(".work_title a").ellipsis({"width":workTitleWidth, "useContainerPadding": true, "useContainerMargin": false});
-}
 
-$(document).ready(function(){
-    layoutWorkPanel();
-    navMenuBuilding();
-    dropDownTimeSelectorForPop();
-    dropDownPlatformSelectorForPop();
-    dropDownTagsSelectorForPop();
-    convertTags();
-=======
-/**
- * The entry of scripting contain menu building.
- * Licensed under Free-BSD
- * Author: Delacstrasz
- * Date: July 15th, 2012
- * Version: 1.0
- */
-
-//lock the menu hover effect when this menu is the  current page
-(function($, undefined){
-    // prevent duplicate loading
-    // this is only a problem because we proxy existing functions
-    // and we don't want to double proxy them
-    $.simpleMenu = $.simpleMenu || {};
-
-    if($.simpleMenu.version){
-        return;
-    }
-
-    $.extend($.simpleMenu, {
-        version: "1.00",
-        hrefList: {},
-        returnDelay: 100
+    $(".category_paginater").paginator({
+        "totalItems": 500,
+        "num_per_page": 16,
+        "currentPage": 0,
+        "num_display_entries": 5,
+        "num_edge_entires": 1,
+        "prev_page_text": "上一页",
+        "next_page_text": "下一页",
+        "ellipsis_text": "...",
+        "callback": page_handle,
+        "ajax": true
     });
-
-    $.fn.extend({
-        lockedMenu:function (returnDelay) {
-            $.simpleMenu.returnDelay = returnDelay || $.simpleMenu.returnDelay;
-            var currentPath = window.location.pathname;
-            var currentURL = currentPath.substring(currentPath.lastIndexOf("/"), currentPath.length);
-            currentURL = currentURL || "/index.html";
-            //consider our default index.html is root /,so if the path is /index.html. convert to root
-            currentURL = currentURL=="/index.html" ? "/" : currentURL;
-            $.extend($.simpleMenu, {
-                defaultURL: currentURL
-            });
-            var menuElement = this;
-            this.find("a").each(function(index,domElement){
-                var $anchor = $(domElement);
-                var href = $anchor.attr("href");
-                if(href==currentURL) {
-                    $.simpleMenu.hrefList[ href ] = true;
-                    $anchor.addClass("hot");
-                } else {
-                    $.simpleMenu.hrefList[ href ] = false;
-                    $anchor.hover(function(event){
-                        activeMenuItem(event, menuElement);
-                    },function(event){
-                        deactiveMenuItem(event, menuElement);
-                    });
-                }
-            });
-        }
-    });
-
-    function activeMenuItem (event, menuElement) {
-        var target = event.target;
-        var $anchor = $(target);
-        $.simpleMenu.hrefList[ $anchor.attr("href") ] = true;
-        $anchor.addClass("hot");
-        // $.simpleMenu.hrefList[$.simpleMenu.defaultURL] = false;
-        // menuElement.find("a[href='"+$.simpleMenu.defaultURL+"']").removeClass("hot");
-        menuElement.find("a[href!='"+$anchor.attr("href")+"']").each(function(i, item){
-            $(item).removeClass("hot");
-            $.simpleMenu.hrefList[$(item).attr("href")] = false;
+    $(".page_button_wrapper").convertToButton();
+    
+    $(".category_paginater").each(function(){
+        var paginator_width = 0;
+        $(this).children().each(function() {
+            var child = $(this);
+            
+            if(child.hasClass("page_ellipsis_wrapper")) {
+                paginator_width+= child.width() + 21;
+            } else {
+                paginator_width+= child.width()+8;
+            }
+            // console.log(paginator_width);
         });
-        // console.log(target);
-    }
-
-    function deactiveMenuItem (event, menuElement) {
-        var target = event.target;
-        var $anchor = $(target);
-        $.simpleMenu.hrefList[ $anchor.attr("href")] = false;
-        $anchor.removeClass("hot");
-        setTimeout(function() {
-            var isMouseLeft = true;
-            $.each($.simpleMenu.hrefList, function(index, item){
-                if(item){
-                    isMouseLeft = false;
-                }
-            });
-            if(isMouseLeft) {
-                $.simpleMenu.hrefList[$.simpleMenu.defaultURL] = true;
-                menuElement.find("a[href='"+$.simpleMenu.defaultURL+"']").addClass("hot");
-            }
-        }, $.simpleMenu.returnDelay);
-    }
-
-})(jQuery);
-
-function navMenuBuilding() {
-    $("#nav_menu").lockedMenu();
-    $("#nav_menu").lavaLamp({
-        speed: 300,
-        click: function(){
-
-        },
-        starItem : 1
+        paginator_width += 20;
+        // console.log(paginator_width);
+        $(this).css({"width":  paginator_width+"px", "margin-left": "auto", "margin-right": "auto"});
     });
-}
+    
 
-function dropDownTimeSelectorForPop() {
-    var api = "/api/time";
-    $("#time_selector_for_pop").jDropDownControl({
-        "click":function(index, event) {
-            var target = event.target;
-            var data = $(target).attr("data");
-            console.log(data);
-            // var time = $.parseJSON(data);
-            $("#time_selector_for_pop div.drop_down_wrapper").children("span").text($(target).text());
-            if(data){
-                // window.location = api+"?time="+data;
-                
-            }
-        }
-    });
-    $("#time_selector_for_new").jDropDownControl({
-        "click":function(index, event) {
-            var target = event.target;
-            var data = $(target).attr("data");
-            console.log(data);
-            // var time = $.parseJSON(data);
-            $("#time_selector_for_new div.drop_down_wrapper").children("span").text($(target).text());
-            if(data){
-                // window.location = api+"?time="+data;
-                
-            }
-        }
-    });
-}
-
-function dropDownPlatformSelectorForPop() {
-    var api="/api/platform";
-    $("#platform_selector_for_pop").jDropDownControl({
-        "click":function(index, event) {
-            var target = event.target;
-            var data = $(target).attr("data");
-            console.log(data);
-            $("#platform_selector_for_pop div.drop_down_wrapper").children("span").text($(target).text());
-            if(data) {
-                // window.location = api+"?platform="+data;
-            }
-        }
-    });
-    $("#platform_selector_for_new").jDropDownControl({
-        "click":function(index, event) {
-            var target = event.target;
-            var data = $(target).attr("data");
-            console.log(data);
-            $("#platform_selector_for_new div.drop_down_wrapper").children("span").text($(target).text());
-            if(data) {
-                // window.location = api+"?platform="+data;
-            }
-        }
-    });
-}
-
-function dropDownTagsSelectorForPop() {
-    var api="/api/tags";
-    $("#tags_selector_for_pop").jDropDownControl({
-        "click":function(index, event) {
-            var target = event.target;
-            var data = $(target).attr("data");
-            console.log(data);
-            if(data) {
-                // window.location = api+"?platform="+data;
-            }
-        },
-        "width": "200px",
-        "defaultLabel": "TAGs"
-    });
-    $("#tags_selector_for_new").jDropDownControl({
-        "click":function(index, event) {
-            var target = event.target;
-            var data = $(target).attr("data");
-            console.log(data);
-            if(data) {
-                // window.location = api+"?platform="+data;
-            }
-        },
-        "width": "200px",
-        "defaultLabel": "TAGs"
-    });
-}
-
-function convertTags() {
+    //convert tags
     $(".work_tags, #tags_selector_for_pop, #tags_selector_for_new").convertToTags({
         "click":function(tag_frame, event){
             var data = tag_frame.attr("data");
@@ -650,33 +272,10 @@ function convertTags() {
     });
 }
 
-function layoutWorkPanel() {
-    //Add margins to work panel, but not the last one of each row.
-    $(".works_panel_wrapper").children(".works_panel").each(function(index, item) {
-        if( (index+1) % 4 != 0 ) {
-            $(item).addClass("works_panel_margin");
-        }
-    });
-    //Add slide animation to preview image light box controls.
-    $(".work_content_wrapper div.preview_img_wrapper").hover(function(event) {
-        $(this).find("div.img_lightbox_controls").slideDown("fast");
-    }, function(event) {
-        $(this).find("div.img_lightbox_controls").slideUp("fast");
-    });
-    //convert the more content link to a relief button.
-    $(".more_content_button_wrapper").convertToButton();
-    //Apply FancyBox to light box controls
-    $(".zoom_in_tool").fancybox();
-
-}
-
 $(document).ready(function(){
     layoutWorkPanel();
     navMenuBuilding();
     dropDownTimeSelectorForPop();
     dropDownPlatformSelectorForPop();
     dropDownTagsSelectorForPop();
-    convertTags();
->>>>>>> Merge pull request #1 from lordfriend/master
->>>>>>> d8ee030a1e74cbf828bf26adb41b64ae3f930e3d
 });
