7274056f2c051fe6200802ebb664f8d5731f368b
Harry Cheng <chengyuhui1@gmail.com>
2012-09-05 17:21:53 +0800

    add new page detail and modified many js files

---
+++ static/js/detail.js
@@ -0,0 +1,8 @@
+function layoutWorkInfo () {
+    $("#project_download_button").convertToButton();
+    $("#project_favorite_button").convertToButton();
+}
+
+$(document).ready(function(){
+    layoutWorkInfo();
+});
